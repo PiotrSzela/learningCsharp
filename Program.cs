89847ec68972cc1a91560d8e356460abae12abbb
Pan Prezes <Pan Prezes@PREZES>
2022-01-24 11:00:32 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many numbers you want to check:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] tab = new int[a];
+
+            Console.WriteLine($"give me {a} numbers:");
+
+            for (int i = 0; i < a; i++)
+            {
+                tab[i] = Convert.ToInt32(Console.ReadLine()); // add number until tab[i] < a
+            }
+
+            Array.Sort(tab);
+            Console.WriteLine("even numbers:");
+            for (int i = 0; i < tab.Length; i++)
+
+
+                if (tab[i] % 2 == 0)
+                    Console.Write($"{tab[i]}, ");
+
+            Console.WriteLine("\n odd numbers:");
+
+            for (int i = 0; i < tab.Length; i++)
+
+                if (tab[i] % 2 == 1)
+                    Console.Write($"{tab[i]}, ");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
